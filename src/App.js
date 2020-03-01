abb8474bf79b93673af74e882978df9caab4f85f
hawk327 <53837808+hawk327@users.noreply.github.com>
2020-02-29 21:31:38 -0500

    Addind react src folder and readme.md file.
     create a react app:
    "npx create-react-app my-app"
    "cd my-app"
    "npm start"

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import './css/admin.css'
+import './css/index.css'
+import './css/login.css'
+import './css/profile.css'
+import './css/signup.css'
+
+// import Sidebar from './components/sidebar'
+import Signup from './components/signup'
+import Index from './components/index'
+// import About from './components/about'
+// import Timeline from './components/timeline'
+import Admin from './components/admin';
+import Profile from './components/profile';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div id="page">
+        <div id="container">
+         	{/* <Sidebar></Sidebar> */}
+				<div id="main">
+					{/* <Signup></Signup> */}
+          {/* <Index></Index> */}
+          <Admin></Admin>
+          {/* <Profile></Profile> */}
+					{/* <About></About> */}
+					{/* <Timeline></Timeline> */}
+          	</div>
+      	</div>
+      </div>
+    );
+  }
+}
+
+export default App;
