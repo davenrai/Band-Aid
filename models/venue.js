0cb33ce13efc90d4c190d8f7a795eb82321039ed
Michael Hawkins <m.hawkins@mail.utoronto.ca>
2020-03-21 01:30:13 -0400

    adding venue model

---
+++ models/venue.js
@@ -0,0 +1,24 @@
+/* Student mongoose model */
+const mongoose = require('mongoose')
+
+const Venue = mongoose.model('Venue', {
+	name: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	},
+	type: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+    }
+    id: {
+		type: Number,
+		required: true,
+		// default: 1
+	}
+})
+
+module.exports = { Venue }
