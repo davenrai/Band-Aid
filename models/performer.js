2b674c208a06a0fa5a05125e4e84b05d979f23fc
hawk327 <53837808+hawk327@users.noreply.github.com>
2020-03-21 01:24:36 -0400

    github webui upload of models folder

---
+++ models/performer.js
@@ -0,0 +1,24 @@
+/* Student mongoose model */
+const mongoose = require('mongoose')
+
+const Performer = mongoose.model('Performer', {
+	name: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	},
+	genre: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+    }
+    id: {
+		type: Number,
+		required: true,
+		// default: 1
+	}
+})
+
+module.exports = { Performer }
