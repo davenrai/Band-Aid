583fb9b5e77f4faace444b9c2015e253aad4da2d
zhaonancy <nancy.zhao@outlook.com>
2020-03-25 22:42:36 -0400

    changes to App.js login user

--- band/src/App.js
+++ band/src/App.js
@@ -4,10 +4,21 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 import Home from './react-components/Home';
-import Login from './react-components/Login'
+import Login from './react-components/Login';
 
+import { readCookie } from "./actions/user";
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    readCookie(this); // sees if a user is logged in.
+  }
+
+  // global state passed down includes the current logged in user.
+  state = {
+    currentUser: null
+  }
   render() {
+    const { currentUser } = this.state;
     return (
       <div className="App">
         <BrowserRouter>
