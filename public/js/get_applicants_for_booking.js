dde1d9b18952227c1decdb39dd84e71f22122b39
Mike <m.hawkins@mail.utoronto.ca>
2020-04-01 09:43:02 -0400

    fixing up code for getting applicants

--- public/js/get_applicants_for_booking.js
+++ public/js/get_applicants_for_booking.js
@@ -6,9 +6,9 @@ const log = console.log
 log('Loaded front-end javascript.')
 
 // A function to send a GET aaplicants for have applied for a booking.
-    function fetchTest5() {
+    function getApplicants() {
         // the URL for the request
-        const url = '/users';
+        const url = '/bookings';
     
         // Since this is a GET request, simply call fetch on the URL
         fetch(url)
@@ -21,44 +21,14 @@ log('Loaded front-end javascript.')
            }                
         })
         .then((json) => {  // the resolved promise with the JSON body
-            usersList = document.querySelector('#usersList')
-            usersList.innerHTML = '';
+            performersList = document.querySelector('#performersList')
+            performersList.innerHTML = '';
             log(json)
-            json.users.map((s) => {
+            json.bookings.map((b) => {
                 li = document.createElement('li')
-                li.innerHTML = `Name: <strong>${s.username}</strong>, Year: <strong>${s.usertype}</strong>`
-                usersList.appendChild(li)
-                log(s)
-            })
-        }).catch((error) => {
-            log(error)
-        })
-    }
-
-    // currently same as fetchTest5
-    function fetchTest6() {
-        // the URL for the request
-        const url = '/users';
-    
-        // Since this is a GET request, simply call fetch on the URL
-        fetch(url)
-        .then((res) => { 
-            if (res.status === 200) {
-                // return a promise that resolves with the JSON body
-               return res.json() 
-           } else {
-                alert('Could not get users')
-           }                
-        })
-        .then((json) => {  // the resolved promise with the JSON body
-            usersList = document.querySelector('#usersList')
-            usersList.innerHTML = '';
-            log(json)
-            json.users.map((s) => {
-                li = document.createElement('li')
-                li.innerHTML = `Name: <strong>${s.username}</strong>, Year: <strong>${s.usertype}</strong>`
-                usersList.appendChild(li)
-                log(s)
+                li.innerHTML = `Venue Name: <strong>${b.venuename}</strong>, Applicants: <strong>${b.applications[0]}</strong>`
+                performersList.appendChild(li)
+                log(b)
             })
         }).catch((error) => {
             log(error)
