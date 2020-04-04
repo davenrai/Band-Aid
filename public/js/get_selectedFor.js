34dc0db5b81a4ef7fdd109189fc6e92d71ffddf3
Mike <m.hawkins@mail.utoronto.ca>
2020-04-04 10:47:57 -0400

    fixed applicant-selectedFor js and html

--- public/js/get_selectedFor.js
+++ public/js/get_selectedFor.js
@@ -24,37 +24,38 @@ function getSelectedFor() {
         log(json);
         log(json.selectedFor)
         json.selectedFor.map((b) => {
-                li = document.createElement('li');
-                const venueTitle = document.createElement("strong");
-                venueTitle.innerText = 'Venue Name: ';
-                li.appendChild(venueTitle);
-                const divVenueName = document.createElement("span");
-                divVenueName.innerHTML = `${b.venuename}`;
-                li.appendChild(divVenueName);
 
 
-                // *** will use code below once selectedFor array holds the date
-                // // use &nbsp (non-breaking line space)  ???
-                const lineSpace1 = document.createTextNode("---");
-                li.appendChild(lineSpace1);
-                const dateTitle = document.createElement("strong");
-                dateTitle.innerText = 'Date: ';
-                li.appendChild(dateTitle);
-                const divBookingDate = document.createElement("span");
-                divBookingDate.innerHTML = `${b.bookingDate}`;
-                li.appendChild(divBookingDate);
+            const newRequest = document.createElement("div");
+            newRequest.className = "request";
+            const reqText = document.createElement("p");
+            const reqTitle = document.createElement("strong");
+            reqTitle.innerHTML = `${b.venuename}`;
+            reqText.appendChild(reqTitle);
+            
+            const lineBreak1 = document.createElement("br");
+            reqText.appendChild(lineBreak1);
 
-                const lineSpace2 = document.createTextNode("---");
-                li.appendChild(lineSpace2);
-                const locationTitle = document.createElement("strong");
-                locationTitle.innerText = 'Location: ';
-                li.appendChild(locationTitle);
-                const divLocation = document.createElement("span");
-                divLocation.innerHTML = `${b.location}`;
-                li.appendChild(divLocation);
-                
-                selectedForList.appendChild(li);
-                log(b);
+            const divLocation = document.createElement("span");
+            divLocation.innerHTML = `${b.location}`;
+            reqText.appendChild(divLocation);
+
+            const lineBreak2 = document.createElement("br");
+            reqText.appendChild(lineBreak2);
+           
+            
+            const divBookingDate = document.createElement("span");
+            divBookingDate.innerHTML = `${b.bookingDate}`;
+            reqText.appendChild(divBookingDate);
+
+            const lineBreak3 = document.createElement("br");
+            reqText.appendChild(lineBreak3);
+            const lineBreak4 = document.createElement("br");
+            reqText.appendChild(lineBreak4);
+
+            newRequest.appendChild(reqText);
+
+            selectedForList.appendChild(newRequest);
         });
     }).catch((error) => {
         log(error);
