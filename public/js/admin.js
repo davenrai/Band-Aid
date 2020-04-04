bd62b95aa4630c7b9ff5c5e6b4923fc289f673df
zhaonancy <nancy.zhao@outlook.com>
2020-04-04 12:24:07 -0400

    admin success/error msgs done

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,5 +1,6 @@
 /* AJAX calls */
 const log = console.log;
+log("loaded admin.js");
 
 // function updatePassword() {
 //     // the URL for the request
@@ -48,13 +49,18 @@ function deleteUser() {
     });
     fetch(request)
         .then(function (res) {
+            const message = document.querySelector('#messageDeleteUser');
             // Handle response we get from the API.
             if (res.status === 200) {
                 // If user was deleted successfully, tell the admin.
                 log('user deleted');
+                message.innerText = `Success: Delete user ${data.username}.`;
+                message.setAttribute("style", "color: white");
             } else {
                 // If server couldn't delete the user, tell the admin.
                 log('user delete error');
+                message.innerText = `Error: Unable to delete user ${data.username}.`;
+                message.setAttribute("style", "color: white");
             }
         }).catch((error) => {
             log(error);
@@ -78,13 +84,18 @@ function deleteBooking() {
     });
     fetch(request)
         .then(function (res) {
+            const message = document.querySelector('#messageDeleteBooking');
             // Handle response we get from the API.
             if (res.status === 200) {
                 // If booking was deleted successfully, tell the admin.
                 log('booking deleted');
+                message.innerText = `Success: Deleted booking for ${data.venuename}.`;
+                message.setAttribute("style", "color: white");
             } else {
                 // If server couldn't delete the booking, tell the admin.
                 log('booking delete error');
+                message.innerText = `Error: Unable to delete booking for ${data.venuename}.`;
+                message.setAttribute("style", "color: white");
             }
         }).catch((error) => {
             log(error);
@@ -112,13 +123,18 @@ function updateBooking() {
     });
     fetch(request)
         .then(function (res) {
+            const message = document.querySelector('#messageUpdateBooking');
             // Handle response we get from the API.
             if (res.status === 200) {
                 // If booking was updated successfully, tell the admin.
                 log('updated booking');
+                message.innerText = `Success: Updated booking for ${data.venuename}.`;
+                message.setAttribute("style", "color: white");
             } else {
                 // If booking couldn't be updated, tell the admin.
                 log('update booking error');
+                message.innerText = `Error: Unable to update booking for ${data.venuename}.`;
+                message.setAttribute("style", "color: white");
             }
         }).catch((error) => {
             log(error);
