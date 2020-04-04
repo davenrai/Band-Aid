cf690ea422fd66f06e8d2382818e5f4acffa3c54
Mike <m.hawkins@mail.utoronto.ca>
2020-04-03 20:40:04 -0400

    now performers selectedFor array has objects only

--- server.js
+++ server.js
@@ -501,7 +501,7 @@ app.post('/users/choosePerformer/:performername', (req, res) => {
 			log("req.body.venuename is: " + req.body.venueame);
 			user.selectedFor.push(req.body);
 			// below code saves an object to 
-			user.selectedFor.push(req.body.venueName);
+			// user.selectedFor.push(req.body.venueName);
 			user.save().then((result) => {
 				// pass the reservation that was just pushed
 				// note that mongoose provided an _id when it was pushed
