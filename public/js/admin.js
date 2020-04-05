d4725e6e3d4d8d5fa3754b5c4a38f4cb81eab9a0
zhaonancy <nancy.zhao@outlook.com>
2020-04-04 20:53:14 -0400

    formatting

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,36 +2,6 @@
 const log = console.log;
 log("loaded admin.js");
 
-// function updatePassword() {
-//     // the URL for the request
-//     const url = '/admin';
-//     let data = {
-//         username: document.querySelector('#username').value,
-//         password: document.querySelector('#password').value,
-//     };
-//     const request = new Request(url, {
-//         method: 'PATCH', 
-//         body: JSON.stringify(data),
-//         headers: {
-//             'Accept': 'application/json, text/plain, */*',
-//             'Content-Type': 'application/json'
-//         },
-//     });
-//     fetch(request)
-//     .then(function(res) {
-//         // Handle response we get from the API.
-//         if (res.status === 200) {
-//             // If password was changed successfully, tell the admin.
-//             log('UPDATED');
-//         } else {
-//             // If password couldn't be changed, tell the admin.
-//             log('didnt work');
-//         }
-//     }).catch((error) => {
-//         log(error);
-//     });
-//}
-
 
 function deleteUser() {
     // the URL for the request
